c741c105375df6b4b0c8d89920ccbe536f03fec6
Emika Hammond <eth@bu.edu>
2022-06-26 16:32:50 -0400

    begin

---
+++ avoiddot.cpp
@@ -0,0 +1,44 @@
+// Copyright 2022 Jacob Chin @bu.edu
+// Copyright 2022 Emika Hammond eth@bu.edu
+// Copyright 2022 Nuo Lin nuolin@bu.edu
+// Copyright 2022 Richard Yang rich@bu.edu
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+// #include <SFML/Audio.hpp>
+#include <ctime>
+#include <cstdlib>
+using std::cin;
+using std::cout;
+using namespace std;
+class Dots {
+    
+public:
+    Dots();
+    ~Dots();
+    int size;
+    int x;
+    int y;
+    int getsize();
+    sf::CircleShape dot;
+    sf::Color;
+    void speedx;
+    void speedy;
+    void dotposition(int x, int y);
+
+    
+};
+
+
+
+// initialize dot commands
+// int Dot::getsize() {
+//     return size;
+// }
+
+Dots::Dots() {
+    // size = 
+}
+
+
+
